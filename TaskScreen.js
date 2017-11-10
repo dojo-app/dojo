3c45eb7220d7ecdcafecfd214f783e54ed37aca9
Xristopher <xsomera@ucsd.edu>
2017-11-10 14:30:08 -0800

    Grabbing newest changes

--- TaskScreen.js
+++ TaskScreen.js
@@ -48,7 +48,7 @@ export class TaskScreen extends React.Component {
           </List>
           <Text>{this.state.text}</Text>
         </Content>
-      </Container>    
+      </Container>
     );
   }
 }
